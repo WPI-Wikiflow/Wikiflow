84d3d9a3e4afe21d9b96a422f3c102e83814891c
GabrielDeml <gabrielddeml@gmail.com>
2022-12-13 12:36:57 -0500

    fix

--- lib/directed_graphview.dart
+++ lib/directed_graphview.dart
@@ -154,7 +154,7 @@ class _GraphClusterViewPageState extends State<GraphClusterViewPage> {
     );
   }
 
-  WikiHelper wikiHelper = WikiHelper("test.csv");
+  WikiHelper wikiHelper = WikiHelper("/Wikiflow/test.csv");
   Graph graph = Graph();
   Algorithm? builder;
   String _articleSearch = "";
