4c7fb8988d182588a118f6e911af052d64335af9
GabrielDeml <gabrielddeml@gmail.com>
2022-12-13 15:01:05 -0500

    stuff

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -3,7 +3,12 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "main.dart.js": "c4457ee48b939e58e1db00df1a212ff7",
+  "canvaskit/canvaskit.js": "97937cb4c2c2073c968525a3e08c86a3",
+"canvaskit/canvaskit.wasm": "3de12d898ec208a5f31362cc00f09b9e",
+"canvaskit/profiling/canvaskit.js": "c21852696bc1cc82e8894d851c01921a",
+"canvaskit/profiling/canvaskit.wasm": "371bc4e204443b0d5e774d64a046eb99",
+"flutter.js": "1cfe996e845b3a8a33f57607e8b09ee4",
+"main.dart.js": "c4457ee48b939e58e1db00df1a212ff7",
 "version.json": "5b29f58095bc2693277942d4a5022a2f",
 "assets/assets/test.csv": "b32c3699b504ea012e1a24f9ea262a86",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "6d342eb68f170c97609e9da345464e5e",
@@ -16,8 +21,8 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "f4565d33ef23f246c676ff9a955e78d1",
-"/": "f4565d33ef23f246c676ff9a955e78d1",
+"index.html": "d7dc5cc385722ed27d05cf399ca2266a",
+"/": "d7dc5cc385722ed27d05cf399ca2266a",
 "manifest.json": "19ca3f36fa2bc94bb4b469865543e949"
 };
 
